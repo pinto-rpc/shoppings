075814834e9afce5a9a356929bb59fb918f2a566
pinto_rpc <amistad.hola@gmail.com>
2021-10-24 15:53:38 -0400

    modificamos el modelo y hemos creado el factory de producto mas sus faker

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'title',
+        'description',
+        'price',
+        'stock',
+        'status',
+    ];
 }
