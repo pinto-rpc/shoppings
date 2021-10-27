40d7f9229ac1880cae24499f02a8dff8a67950d3
pinto_rpc <amistad.hola@gmail.com>
2021-10-26 23:06:29 -0400

    se ha creado el product factoy mas sus caracteristicas y el seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -23,7 +23,7 @@ class ProductFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence(3),
-            'description' => $this->faker->paragraph(1),
+            'description' => $this->faker->paragraph(2),
             'price' => $this->faker->randomFloat($maxDecimals=2, $min=3, $max=100),
             'stock' => $this->faker->numberBetween(1, 10),
             'status' => $this->faker->randomElement(['available', 'unavailable']),
