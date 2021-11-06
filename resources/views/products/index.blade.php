e6c4bdfa77726d8babcf721dbc2b43055cb3b295
pinto_rpc <amistad.hola@gmail.com>
2021-11-06 01:12:48 -0400

    se trabajo con blade se ha creado una carpeta layouts y archivo master

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lista de Productos</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
     <h1>Productos</h1>
     @empty($products)
     <h1>No hay ningun producto aun registrado</h1>
@@ -36,5 +29,4 @@
         </tbody>
       </table>
       @endempty
-</body>
-</html>
+@endsection
